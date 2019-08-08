6431560956bf284898385ac1e8a67af58af84c24
mukesh <mukeshmahara1@gmai.com>
2019-08-08 18:20:54 +0545

    Add project files.

---
+++ MVC-pro/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using MVC_pro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_pro.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            Student model = new Student();
+            model.Name = "Mukesh";
+            return View(model);
+        }
+    }
+}
---
+++ MVC-pro/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_pro.Models
+{
+    public class Student
+
+    {
+        public string Name { get; set; }
+
+    }
+    
+}
